8c8c6428f8f05c397517a02076f2287e4b5eef90
sujeetkumar07 <sujeet.kumar.aot07@gmail.com>
2025-02-16 14:52:17 +0530

    get put delete

--- src/app.js
+++ src/app.js
@@ -2,12 +2,24 @@ const express = require('express');
 const app = express();
 
 
-app.use("/hello",(req,res)=>{
+app.use("user",(req,res)=>{
+    res.send("Heyyyyyyyyyyy")
+})
+//This will only handle GET call to users
+app.get("/user",(req,res)=>{
+    res.send({firstName: "Sujeet", lastName:"Kumar"})
+});
+app.post("/user",(req,res)=>{
+    // console.log("save data to the database");
+    res.send("saving the data to database");
+});
+app.delete("/user",(req,res)=>{
+    res.send("deleted successfully");
+});
+//this will match all the HTTP method API calls to /test
+app.use("/test",(req,res)=>{
     res.send("hello")
 })
-app.use("/test",(req,res)=>{
-    res.send("Hello send from the server that is running at 3000")
-})
-app.listen(3000,()=>{
+app.listen(7777,()=>{
     console.log('server is successfully running at 3000');
 });
